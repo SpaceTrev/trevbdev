297feefef3bad9ead23be55794b638dd1f110e02
Trevor Benavides <gratefultrev@gmail.com>
2019-07-10 00:37:30 -0600

    Updates

---
+++ precache-manifest.2a6c3c9d1cc454130d33dec7598a633b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a38670a3e72cd274cd236aedb34caf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bca99cdcbf4b7c43e62",
+    "url": "/static/css/2.bb27b9fa.chunk.css"
+  },
+  {
+    "revision": "ecc861b2e9166130524e",
+    "url": "/static/css/main.a15c4edf.chunk.css"
+  },
+  {
+    "revision": "8bca99cdcbf4b7c43e62",
+    "url": "/static/js/2.b59869a0.chunk.js"
+  },
+  {
+    "revision": "ecc861b2e9166130524e",
+    "url": "/static/js/main.17218fa7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e5f8bc61c90513547002edb2b1771cf1",
+    "url": "/static/media/friendfinder.e5f8bc61.mov"
+  },
+  {
+    "revision": "fcb70bed5f2dac2b47df90f2f5502ad1",
+    "url": "/static/media/mssdemo.fcb70bed.mov"
+  },
+  {
+    "revision": "5a5994c15fc1986d46c9eed71423e415",
+    "url": "/static/media/tface.5a5994c1.jpg"
+  }
+]);
